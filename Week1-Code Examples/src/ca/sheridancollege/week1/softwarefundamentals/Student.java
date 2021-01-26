88142c81fd3dfa0a90c6a0da711930f19a566bb6
Karan <Karan@MSI>
2021-01-26 22:08:25 +0530

    My FIRST commit

--- Week1-Code Examples/src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ Week1-Code Examples/src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -1,6 +1,7 @@
 /*
- * This is my first commit
-Testing .gitignore feature
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
