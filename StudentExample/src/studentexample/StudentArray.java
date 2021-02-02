8121b8433f6243479601c62493c6112a5345296f
Karan <Karan@MSI>
2021-02-02 20:57:42 +0530

    The first one

---
+++ StudentExample/src/studentexample/StudentArray.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentexample;
+
+/**
+ *
+ * @author Karan
+ */
+public class StudentArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
