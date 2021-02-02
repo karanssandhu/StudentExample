b5d8cf66dfd324898033812bd8621b2e9cae3869
Karan <Karan@MSI>
2021-02-02 21:18:33 +0530

    Commit#4

--- StudentExample/src/studentexample/Student.java
+++ StudentExample/src/studentexample/Student.java
@@ -1,9 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * New comment commit #4
  */
-package ca.sheridancollege.week1.softwarefundamentals;
+package studentexample;
 
 /**
  *
