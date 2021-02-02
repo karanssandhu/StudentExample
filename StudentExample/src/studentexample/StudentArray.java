7730117b60b1851f5670de93f60d42d8a802ec76
Karan <Karan@MSI>
2021-02-02 21:44:22 +0530

    Commit#4

--- StudentExample/src/studentexample/StudentArray.java
+++ StudentExample/src/studentexample/StudentArray.java
@@ -1,21 +1,28 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Change to the remote repository #commit 2
+ *Commit#3
  */
-package studentexample;
+package StudentExample;
 
 /**
  *
- * @author Karan
+ * @author Ali Hassan
  */
 public class StudentArray {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Student[] studentList = new Student[5];
+        System.out.println("Hello world");
+        Student student1 = new Student("1", "Jim Halpern");
+        studentList[0] = student1;
+        studentList[1] = new Student("2", "Michael Scott");
+        studentList[2] = new Student("3", "Pam Beasley");
+        studentList[3] = new Student("4", "Dwight Schrute");
+        studentList[4] = new Student("5", "Toby Flendersen");
+
+        for (int i = 0; i < studentList.length; i++) {
+            System.out.println(studentList[i].getName());
+        }
     }
-    
+
 }
---
+++ StudentExample/src/studentexample/partTimeStudent.java
@@ -0,0 +1,17 @@
+/*
+ * Adding code on partTimeStudent commit#4
+*/
+
+package studentexample;
+
+/**
+ *
+ * @author Karan
+ */
+public class partTimeStudent extends Student{
+    
+    public partTimeStudent(String givenID, String givenName){
+        super(givenID, givenName);
+    }
+    
+}
